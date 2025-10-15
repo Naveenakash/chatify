1d0b1920df9ed1084e2fe47a5627ab185bfb3a57
Ganapathy <naveen.a.ganapathy@accenture.com>
2025-10-15 15:22:01 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoute from '../routes/auth.route.js';
+import path from "path";
 
 dotenv.config()
 
 const app = express();
 
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 3000;
 
 app.use('/api/auth', authRoute);
 
+//make ready for deployment
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get('*', (_,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
 app.listen(PORT,()=>console.log("server is running on port" + " " + PORT));
